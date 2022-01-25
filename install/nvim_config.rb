8d6409a6567e0a0cf6e55d92c3947d78288c09e7
Shounak Amladi <shounak.p.amladi@gmail.com>
2022-01-25 17:12:41 -0500

    add winsdows install location

--- install/nvim_config.rb
+++ install/nvim_config.rb
@@ -1,10 +1,9 @@
+require 'os'
 module Config
-  
-  CONFIG_PATH = '~/.config/nvim/init.vim'.freeze
-
-  def Config.sync(file_path)
-    File.symlink(file_path, File.expand_path(CONFIG_PATH))
+  CONFIG_PATH_LIN = '~/.config/nvim/init.vim'.freeze
+  CONFIG_PATH_WIN = '~/AppData/Local/nvim/init.vim'.freeze  
+  def self.sync(file_path)
+    File.symlink(file_path, File.expand_path(CONFIG_PATH_LIN)) unless OS.windows?
+    File.symlink(file_path, File.expand_path(CONFIG_PATH_WIN)) if OS.windows?
   end
-
 end
-
